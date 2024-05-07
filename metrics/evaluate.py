ff6eb212df9bbfbe361c09209f443e37949d4eb1
Daniil Kirillov <dikirillov@edu.hse.ru>
2024-05-07 20:07:36 +0300

    add evaluation function

---
+++ metrics/evaluate.py
@@ -0,0 +1,10 @@
+from sklearn import metrics
+from accuracy import calc_accuracy
+
+
+def evaluate(y_true, predictions, num_clusters):
+    acc = calc_accuracy(y_true, predictions, num_clusters)
+    nmi = metrics.normalized_mutual_info_score(y_true, predictions)
+    ari = metrics.adjusted_rand_score(y_true, predictions)
+    fmi = metrics.fowlkes_mallows_score(y_true, predictions)
+    return acc, nmi, ari, fmi
