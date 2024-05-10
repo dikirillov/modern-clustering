f96db9cf7c34acd2a58bab9fde0f015990caeafc
Daniil Kirillov <dikirillov@edu.hse.ru>
2024-05-10 17:01:07 +0300

    fix cc bugs

--- cc/model.py
+++ cc/model.py
@@ -7,73 +7,22 @@ from tqdm.notebook import tqdm
 from transforms import CCTransforms
 from loss import CCLoss
 
-class CCLoss(nn.Module):
-    def __init__(self, temperature, batch_size, num_clusters, device, alpha=0.5):
-        super().__init__()
-        self.temperature = temperature
-        self.alpha = alpha
-        self.batch_size = batch_size
-        self.num_clusters = num_clusters
-        self.device = device
-
-        self.instance_loss_mask = self.create_mask(batch_size)
-        self.cluster_loss_mask = self.create_mask(num_clusters)
-        self.criterion = nn.CrossEntropyLoss(reduction="sum")
-
-    def create_mask(self, n):
-        mask = torch.ones((n, n))
-        mask = mask.fill_diagonal_(0)
-        mask = torch.cat((mask, mask))
-        return torch.cat((mask, mask), dim=1).bool().to(self.device)
-
-    def instance_loss(self, similarities, mask):
-        N = mask.shape[0]
-        positive_1 = torch.diag(similarities, int(N // 2))
-        positive_2 = torch.diag(similarities, -int(N // 2))
-        positives = torch.cat((positive_1, positive_2), dim=0).to(self.device)
-        positives = positives.reshape(N, 1)
-        negatives = similarities[mask].reshape(N, -1)
-        # loss = (torch.exp(positives) / similarities.sum()).sum()
-        labels = torch.zeros(N).to(self.device).long()
-        logits = torch.cat((positives, negatives), dim=1).to(self.device)
-        loss = self.criterion(logits, labels) / N
-
-        return loss
-
-    def calc_log_loss(self, c):
-        p = c.sum(0).view(-1).to(self.device)
-        p /= p.sum()
-        return (p * torch.log(p)).sum()
-
-    def cluster_loss(self, c1, c2):
-        similarity_f = nn.CosineSimilarity(dim=2)
-        c = torch.cat((c1.t(), c2.t()), dim=0)
-        similarities = similarity_f(c.unsqueeze(1), c.unsqueeze(0)) / self.temperature
-        loss = self.instance_loss(similarities, self.cluster_loss_mask)
-        h_loss = self.calc_log_loss(c1) + self.calc_log_loss(c2)
-        return loss + h_loss
-
-    def forward(self, x1, c1, x2, c2):
-        cur_batch = torch.cat((x1, x2))
-        similarities = (cur_batch @ cur_batch.T / self.temperature).to(self.device)
-
-        return self.alpha * self.instance_loss(similarities, self.instance_loss_mask) + (1 - self.alpha) * self.cluster_loss(c1, c2)
-
 
 class ContrastiveClustering(nn.Module):
-    def __init__(self, n_clusters, hidden_dim):
+    def __init__(self, n_clusters, hidden_dim, in_channels=1):
         super().__init__()
         self.resnet = torchvision.models.resnet34(weights=ResNet34_Weights.IMAGENET1K_V1)
-        # for param in self.resnet.parameters():
-        #     param.requires_grad = False
+        self.in_channels = in_channels
 
         self.instances_projector = nn.Sequential(
             nn.Linear(1000, 1000),
+            nn.BatchNorm1d(1000),
             nn.ReLU(),
             nn.Linear(1000, hidden_dim),
         )
         self.cluster_projector = nn.Sequential(
             nn.Linear(1000, 1000),
+            nn.BatchNorm1d(1000),
             nn.ReLU(),
             nn.Linear(1000, n_clusters),
             nn.Softmax(dim=1),
@@ -83,8 +32,10 @@ class ContrastiveClustering(nn.Module):
         self.hidden_dim = hidden_dim
 
     def forward(self, x):
-        # x_for_resnet = torch.cat((x, x, x), dim=1).to(self.device)
         x_for_resnet = x
+        if self.in_channels == 1:
+            x_for_resnet = torch.cat((x, x, x), dim=1).to(self.device)
+
         resnet_features = self.resnet(x_for_resnet)
         hidden_features = self.instances_projector(resnet_features)
         hidden_features = torch.nn.functional.normalize(hidden_features, dim=1)
@@ -92,8 +43,10 @@ class ContrastiveClustering(nn.Module):
         return hidden_features, cluster_assignment
 
     def get_cluster(self, x):
-        # x_for_resnet = torch.cat((x, x, x), dim=1).to(self.device)
         x_for_resnet = x
+        if self.in_channels == 1:
+            x_for_resnet = torch.cat((x, x, x), dim=1).to(self.device)
+
         resnet_features = self.resnet(x_for_resnet)
         cluster_assignment = self.cluster_projector(resnet_features)
         return cluster_assignment.argmax(dim=1)
@@ -132,17 +85,35 @@ class ContrastiveClustering(nn.Module):
 
         return total_loss / len(data_loader)
 
-    def fit(self, train_loader, val_loader, optimizer, n_epochs, device, dataloader_mode="unsupervised"):
+    def get_metrics(self, dataloader, device):
+        y_true, predictions = [], []
+        for (x1, x2), labels  in tqdm(dataloader):
+            x1 = x1.to(device)
+            predictions += self.get_cluster(x1).tolist()
+            y_true += labels.tolist()
+        predictions = np.asarray(predictions)
+        y_true = np.asarray(y_true)
+        output = evaluate(y_true, predictions, 10)
+        return output
+
+    def fit(self, train_loader, val_loader, optimizer, n_epochs, device, dataloader_mode="unsupervised", path="/kaggle/working/cc"):
         self.loss_fn = CCLoss(temperature=1, batch_size=train_loader.batch_size, num_clusters=self.n_clusters, device=device, alpha=0.5)
         self.device = device
 
         train_loss_history, valid_loss_history = [], []
         epoch = 0
+        accs, nmis, aris, fmis, bcs = [], [], [], [], []
         for epoch in tqdm(range(n_epochs), desc=f'Training {epoch}/{n_epochs}'):
             train_loss = self.training_epoch(train_loader, optimizer, device, dataloader_mode)
-            valid_loss = self.evaluate(valid_loader, device, dataloader_mode)
+            valid_loss = self.evaluate(val_loader, device, dataloader_mode)
 
             train_loss_history.append(train_loss)
             valid_loss_history.append(valid_loss)
+            acc, nmi, ari, fmi, bc = self.get_metrics(val_loader, device)
+            accs.append(acc)
+            nmis.append(nmi)
+            aris.append(ari)
+            fmis.append(fmi)
+            bcs.append(bc)
 
-        return train_loss_history, valid_loss_history
+        return train_loss_history, valid_loss_history, accs, nmis, aris, fmis, bcs
--- cc/transforms.py
+++ cc/transforms.py
@@ -3,7 +3,7 @@ import torchvision.transforms.v2
 
 
 class CCTransforms:
-    def __init__(self, size, mean=None, std=None):
+    def __init__(self, size, mean=None, std=None, is_test=False):
         self.transforms = torchvision.transforms.Compose(
             [
                 torchvision.transforms.RandomResizedCrop(size=size),
@@ -13,9 +13,18 @@ class CCTransforms:
                 torchvision.transforms.RandomGrayscale(p=0.2),
                 torchvision.transforms.v2.GaussianBlur(kernel_size=23),
                 torchvision.transforms.ToTensor(),
-                # torchvision.transforms.Normalize(mean=mean, std=std)
+                torchvision.transforms.Normalize(mean=0.5, std=0.5)
             ]
         )
 
+        if is_test:
+            self.transforms = torchvision.transforms.Compose(
+                [
+                    T.ToTensor(),
+                    T.Resize((32, 32)),
+                    T.Normalize([0.5], [0.5])
+                ]
+            )
+                
     def __call__(self, x):
         return self.transforms(x), self.transforms(x)
